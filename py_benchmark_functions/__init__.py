fa886e7c9fe858cf30b8f97330a1dffe5798fbbd
Moésio Filho <moesio.f@gmail.com>
2025-05-14 10:56:25 -0300

    feat: add functional factory API

--- py_benchmark_functions/__init__.py
+++ py_benchmark_functions/__init__.py
@@ -1,2 +1,9 @@
 from .core import Domain, Function, Metadata, Transformation
-from .factory import Builder
+from .factory import (
+    Builder,
+    available_backends,
+    available_functions,
+    get_fn,
+    get_np_function,
+    get_tf_function,
+)
--- py_benchmark_functions/factory/__init__.py
+++ py_benchmark_functions/factory/__init__.py
@@ -1,2 +1,29 @@
+from py_benchmark_functions import Function
+
 from .builder import Builder
 from .registry import Registry
+
+
+# Functional API
+def get_fn(fn_name: str, dims: int) -> Function:
+    return Builder().function(fn_name).dims(dims).build()
+
+
+def get_np_function(fn_name: str, dims: int) -> Function:
+    return Builder().function(fn_name).numpy().dims(dims).build()
+
+
+def get_tf_function(fn_name: str, dims: int) -> Function:
+    return Builder().function(fn_name).tensorflow().dims(dims).build()
+
+
+def available_backends() -> set[str]:
+    return Registry.backends
+
+
+def available_functions() -> list[str]:
+    return Registry.functions
+
+
+# Clear namescope
+del Function
